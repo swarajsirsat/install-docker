f3202236fb0b7d104018dc756aa73c7d0bd94625
swarajsirsat <85947589+swarajsirsat@users.noreply.github.com>
2022-06-16 10:58:20 +0530

    Update email.py

--- email.py
+++ email.py
@@ -7,5 +7,7 @@ hostname = "smtp.gmail.com"
 with smtplib.SMTP(host=hostname) as connection:
       connection.starttls()
       connection.login(user=email, password=password)
-      
-        connection.sendmail(from_addr=email,to_addrs="suvashsirsat123@gmail.com", msg="Subject: Test\n\n This is test email")
+
+        connection.sendmail(from_addr=email,
+                           to_addrs="suvashsirsat123@gmail.com",
+                           msg="Subject: Test\n\n This is test email")
